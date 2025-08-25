14841f1971a47a0e0a478c830de2daf63a767e47
Prateek <prateek.kumar.ind@gmail.com>
2025-08-25 12:32:09 +0530

    add services section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -12,7 +13,8 @@ const App = () => {
     <main className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
-      <TrustedBy/>
+      <TrustedBy />
+      <Services />
     </main>
   );
 };
---
+++ src/components/Title.jsx
@@ -0,0 +1,19 @@
+
+
+const Title = ({title, description}) => {
+  return (
+    <div className="flex flex-col items-center gap-4 ">
+      <h3 className="text-3xl sm:text-5xl font-medium mt-8">
+       {title}
+      </h3>
+      <p
+        className="max-w-lg text-center text-gray-500
+      dark:text-white/70 mb-6 "
+      >
+       {description}
+      </p>
+    </div>
+  );
+};
+
+export default Title;
