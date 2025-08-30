1e7be5140d4780481f79229650a41313fce68b9e
Prateek <prateek.kumar.ind@gmail.com>
2025-08-30 19:23:24 +0530

    animate contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
 import Title from "./Title";
 import assets from "../assets/assets";
 import toast from "react-hot-toast";
+import { motion } from "motion/react";
 
 const ContactUs = () => {
   const onSubmit = async (event) => {
@@ -29,7 +29,11 @@ const ContactUs = () => {
   };
 
   return (
-    <div
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ staggerChildren: 0.2 }}
       id="contact-us"
       className="flex flex-col items-center pt-30
     gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 text-gray-700 dark:text-white
@@ -40,7 +44,11 @@ const ContactUs = () => {
         description="From strategy to execution, we craft digital solutions that move your business forward."
       />
 
-      <form
+      <motion.form
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        viewport={{ once: true }}
         className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full"
         onSubmit={onSubmit}
       >
@@ -90,8 +98,8 @@ const ContactUs = () => {
           Submit
           <img src={assets.arrow_icon} className="w-4" alt="" />
         </button>
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   );
 };
 
